a96bde8df3fb126d6a0c471ea8d0af3b0a8b4315
Ian Gao <Ian.Gao@impgroup.com.au>
2019-10-09 13:05:51 +0800

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public List<string> options = new List<string>();
+
+        public HomeController()
+        {
+            options.Add("Malay");
+            options.Add("KFC");
+            options.Add("Maccs");
+            options.Add("Golden Bakery");
+            options.Add("IKEA");
+            options.Add("Food Court");
+        }
+
+        public ActionResult DecideLunchPlace()
+        {
+            Random random = new Random();
+            String Value = options.ElementAt(random.Next(0, options.Count-1));
+            Value = "Vietnamnese";
+            return Json(Value, JsonRequestBehavior.AllowGet);
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Scripts/Random_Place_To_Eat.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $('#PlaceToEat').text("fuck you levi");
+    $('#btn').click(function () {
+
+        $.ajax({
+            type: "Post",
+            url: '/Home/DecideLunchPlace',
+            dataType: "json",
+            success: function (data) {
+                //alert(data);
+                $('#PlaceToEat').text(data);
+            },
+            error: function () {
+                alert("Something wrong");
+            } 
+        });
+    });
+});
